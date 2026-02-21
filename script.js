b0f3200ff63b71a173a35b2497557a46b8cbf5eb
Vince Pag-ong <vince.pagong@gmail.com>
2026-02-21 14:54:34 -0700

    updated2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const messages = [
   "Wait… karon ba? humana naman cguru? 🤔",
   "the 14th oi!💍🏝️ just kidding, no pressure 😜",
   "the 19th? 😎 pero ga bulag bulag man ta 💔hahaha kadaghan bai dates!",
-  "25? 📱 hahahaha TINDER! 🎸guitarist yarn?",
+  "25? 📱 hahahaha TINDER! 🎸'guitarist yarn?'",
   "📅 7th wahahaha first date ba or 8th or 9th 😂? 🍝🍲",
   "It’s the 22nd! HAPPY BIRTHDAY🎂 Hahahaa joke… but who cares? ❤️",
   "Anyways, Its never too early or too late to celebrate us! 💻--🌍--💻",
