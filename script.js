6d220d02e37dfc6f26ba0c0b7bf6d2682c12ff65
Vince Pag-ong <vince.pagong@gmail.com>
2026-02-21 13:17:37 -0700

    semi final

--- script.js
+++ script.js
@@ -1,49 +1,64 @@
+// Messages in order (looping)
 const messages = [
-  {
-    text: "Wait… karon ba? humana naman cguru? 🤔",
-    img: "" // add link
-  },
-  {
-    text: "the 14th oi!💍🏝️ just kidding, no pressure 😜",
-    img: ""
-  },
-  {
-    text: "the 19th? 😎 pero ga bulag man ta hahaha kadaghan ba!",
-    img: ""
-  },
-  {
-    text: "It’s the 22nd! HAPPY BIRTHDAY🎂 Hahahaa joke… but who cares? ❤️",
-    img: ""
-  },
-  {
-    text: "Anyways, Its never too early or too late to celebrate us! 💻❤️",
-    img: ""
-  }
+  "Wait… karon ba? humana naman cguru? 🤔",
+  "the 14th oi!💍🏝️ just kidding, no pressure 😜",
+  "the 19th? 😎 pero ga bulag man ta hahaha kadaghan bai dates!",
+  "25? 📱 hahahaha TINDER!",
+  "It’s the 22nd! HAPPY BIRTHDAY🎂 Hahahaa joke… but who cares? ❤️",
+  "Anyways, Its never too early or too late to celebrate us! 💻--🌍--💻",
+  "Happy Mot Mot Bebu ❤️"
 ];
 
+// Random image pool: images/1.jpg ... images/18.jpg
+const imagePool = [];
+for (let i = 1; i <= 18; i++) {
+  imagePool.push(`images/${i}.jpg`);
+}
+
 let index = -1;
+let lastImage = "";
+let firstClick = true;
+
+// Random image, but don't repeat twice in a row
+function getRandomImage() {
+  let img;
+  do {
+    img = imagePool[Math.floor(Math.random() * imagePool.length)];
+  } while (img === lastImage && imagePool.length > 1);
+
+  lastImage = img;
+  return img;
+}
 
 function nextMessage() {
   const card = document.getElementById("card");
   const msg = document.getElementById("message");
   const img = document.getElementById("memoryImage");
+  const frame = document.getElementById("imageFrame");
+  const title = document.getElementById("titleText");
 
-  // fade out
+  // Hide the title only once (first click)
+  if (firstClick && title) {
+    title.classList.add("hide-title");
+    setTimeout(() => {
+      title.style.display = "none";
+    }, 400);
+    firstClick = false;
+  }
+
+  // Fade out
   card.classList.add("fade-out");
 
   setTimeout(() => {
+    // Advance message in order
     index = (index + 1) % messages.length;
+    msg.innerText = messages[index];
 
-    msg.innerText = messages[index].text;
+    // Random image each click
+    img.src = getRandomImage();
+    frame.style.display = "block";
 
-    if (messages[index].img) {
-      img.src = messages[index].img;
-      img.style.display = "block";
-    } else {
-      img.style.display = "none";
-    }
-
-    // fade in
+    // Fade in
     card.classList.remove("fade-out");
   }, 400);
 }
